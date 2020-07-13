d7763f768e5c43e964496f45411cf689dfaafe56
Blade <bladerboles@gmail.com>
2020-07-13 14:47:33 -0600

    Install axios.  Set up getCharacters function to fetch from backend.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="toggle='character-viewer'">View all characters</button>
+    <button v-on:click="toggle='character-creator'">Create a character</button>
+    <CharacterViewer v-show="toggle==='character-viewer'" />
+    <CharacterCreator v-show="toggle==='character-creator'" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CharacterViewer from './components/CharacterViewer.vue'
+import CharacterCreator from './components/CharacterCreator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CharacterViewer,
+    CharacterCreator
+  },
+  data: function() {
+    return {
+      toggle: "character-viewer"  
+    }  
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    text-align: center
+  }
 </style>
---
+++ src/components/CharacterViewer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="character-view">
+    <h1>Character Viewer</h1>
+    <p>{{ characters }}</p>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'CharacterViewer',
+    data: function() {
+      return {
+        characters: null
+      }
+  },
+  methods: {
+    getCharacters: function() {
+      axios
+        .get('http://localhost:3000/characters')
+        .then(response => (this.characters = response.data))
+    }
+    },
+  mounted: function () {
+    this.getCharacters();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
